1e28aa0dae034287395730f5e6e2efad3dc47f0e
victor truong <2truong93@gmail.com>
2019-05-09 11:36:11 -0400

    turns initialState to parse from localStorage if exists

--- src/store.js
+++ src/store.js
@@ -12,8 +12,17 @@ const rootReducer = combineReducers({
     posts
 });
 
+// check localStorage for any previously saved app state
+// if it's there, pass as second argument to createStore
+let initialState = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+
+// const dataFromLocalStorage = JSON.parse(localStorage.getItem(LS_KEY));
+// if (dataFromLocalStorage){
+//     initialState = dataFromLocalStorage;
+// }
 const store = createStore(
     rootReducer,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
