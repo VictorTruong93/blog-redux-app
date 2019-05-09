42a9a48c7508893babaa558eb0bf7420d2293f34
victor truong <2truong93@gmail.com>
2019-05-09 10:34:38 -0400

    adds post reducer with random id

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -5,6 +5,8 @@ import{
 } from '../actions';
 import { bigIntLiteral } from '@babel/types';
 
+import {generateId} from '../utils';
+
 // A reducer is a function that accepts current state and an action
 // then calculates/returns the next new version of state
 
@@ -13,7 +15,19 @@ export function posts(state={}, action={type: ''}){
     switch(action.type) {
         case ACTION_CREATE_POST:
         // create post
-        console.log(`you called create post action`)
+        // console.log(`you called create post action`)
+        const id = generateId()
+        // console.log(`new id is: ${id}`)
+        const newState={
+            ...state,
+            // to use a variable as a key in object literal,
+            // wrap the veriable in square brackets
+            [id]: action.payload
+        }
+        // to get the equivalent result
+        // use the familiar assignment syntax:
+        // newState[id] = action.payload;
+        return newState;
         break;
         default:
             return state;
