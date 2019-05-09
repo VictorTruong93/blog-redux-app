0f9dbab1fb1c4d9d9578f0c42fd163e32db3a358
victor truong <2truong93@gmail.com>
2019-05-09 09:58:45 -0400

    adds store that uses post reducer

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import {
+    combineReducers,
+    createStore
+} from 'redux';
+
+import {posts} from './reducers/posts';
+
+// Pass an object to combineReducers
+// Object should be shaped like state.
+
+const rootReducer = combineReducers({
+    posts
+});
+
+export default const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
